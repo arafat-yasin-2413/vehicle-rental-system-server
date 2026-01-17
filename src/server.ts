53ab3cbde6ca3be84ebbce4a663f9a2761dc2ff7
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-17 17:05:38 +0600

    server and app file separated

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
-import initDB from "./config/db";
+import app from "./app";
 import config from "./config";
-import logger from "./middleware/logger";
-import { authRoutes } from "./modules/auth/auth.routes";
-import { usersRoutes } from "./modules/users/users.routes";
 
-// express app
-const app = express();
+
 const port = config.port;
-// body parser
-app.use(express.json());
-
-// database setup
-initDB();
-
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", usersRoutes);
-
-app.get("/", logger, (req: Request, res: Response) => {
-    res.send("hello world");
-});
-
-app.post("/", (req: Request, res: Response) => {
-    console.log(req.body);
-
-    res.status(200).json({
-        success: true,
-        message: "Post API is working",
-    });
-});
-
-// Not found route
-app.use((req: Request, res: Response) => {
-    res.status(404).json({
-        success: false,
-        message: "Route not found",
-        path: req.path,
-    });
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
