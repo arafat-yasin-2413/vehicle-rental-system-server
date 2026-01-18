fef99409fe1a3d25e93d1ba13e1bbc839e7b821e
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-18 10:41:16 +0600

    auth constant added

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { usersController } from "./users.controller";
 import auth from "../../middleware/auth";
+import { Roles } from "../auth/auth.constant";
 
 const router = Router();
 
-router.get("/",auth("admin"), usersController.getAllUser);  
+router.get("/",auth(Roles.admin), usersController.getAllUser);  
 
 export const usersRoutes = router;
--- src/modules/vehicles/vehicles.routes.ts
+++ src/modules/vehicles/vehicles.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
 import { vehiclesController } from "./vehicles.controller";
+import { Roles } from "../auth/auth.constant";
 
 const router = Router();
 
-router.post("/", auth("admin"), vehiclesController.addNewVehicle);
+router.post("/", auth(Roles.admin), vehiclesController.addNewVehicle);
 router.get("/", vehiclesController.getAllVehicles);
 router.get("/:vehicleId", vehiclesController.getVehicleById);
-router.put("/:vehicleId", auth("admin"), vehiclesController.updateVehicleById);
-router.delete("/:vehicleId", auth("admin"), vehiclesController.deleteVehicleById);
+router.put("/:vehicleId", auth(Roles.admin), vehiclesController.updateVehicleById);
+router.delete("/:vehicleId", auth(Roles.admin), vehiclesController.deleteVehicleById);
 
 export const vehiclesRoutes = router;
