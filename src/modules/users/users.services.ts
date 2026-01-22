2389b95a353cada4015e380319a7e9079c0e2923
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-22 10:22:42 +0600

    feat: adds update user feature for admin and customer role

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import { pool } from "../../config/db";
 
 const getAllUser = async () => {
@@ -7,25 +8,50 @@ const getAllUser = async () => {
 };
 
 const updateUserProfile = async (id: string, loggedInUserData: Record<string, unknown>, payload: Record<string, unknown>) => {
-    // console.log('I am from user.service file --- update user profile\n', id,"\n",loggedInUserData, "\n", "Payload data \n", payload);
+    // console.log('Logged in user : ', loggedInUserData);
 
     // At first CHECK if the user exist (with "id");
     const matchedUser = await pool.query(`SELECT id,name,email,phone,role FROM users WHERE id=$1`, [id]);
-    
-    console.log("$$$$$$$$ Printing MatchedUser : ", matchedUser.rowCount, matchedUser.rows[0]);
 
     if (matchedUser.rowCount === 0) {
         throw new Error(`No User Found with userId = ${id}`);
-    } else {
-        // if the loggedin user'r role is Admin, He can update the "matchedUser" data with payload, Role === Admin
-       // Admin can update all fields
-        
-        // Role === Customer
-
-        // i) can't update the role
-
-        return "result string";
     }
+
+    const targetedUser = matchedUser.rows[0];
+    // console.log('targetedUser = ', targetedUser, "\n loggedInUserData.role = ", loggedInUserData.role);
+
+    // Logged in User's role is customer
+    if (loggedInUserData.role !== "admin") {
+        const paramsId = Number(id);
+        delete payload.role;
+
+        if (loggedInUserData.id !== paramsId) {
+            // console.log('Payload after validation : ', payload);
+            // console.log('log id : ', typeof loggedInUserData.id, "\n params id : ", typeof paramsId);
+            throw new Error("You are not allowed to update other user's profile.");
+        }
+
+        // now update here
+        // console.log("Payload after validation : ", payload);
+        let hashedPassword = "";
+        if (payload.password) {
+            hashedPassword = await bcrypt.hash(payload.password as string, 12);
+        }
+
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2, password=$3, phone=$4 WHERE id=$5 RETURNING *`, [payload.name, payload.email, hashedPassword, payload.phone, id]);
+        return result;
+    }
+
+    const hashedPasswordByAdmin = await bcrypt.hash(payload.password as string, 12);
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2, password=$3, phone=$4, role=$5 WHERE id=$6 RETURNING *`, [
+        payload.name,
+        payload.email,
+        hashedPasswordByAdmin,
+        payload.phone,
+        payload.role,
+        id,
+    ]);
+    return result;
 };
 
 export const usersServices = {
